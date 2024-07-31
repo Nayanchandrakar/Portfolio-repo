f392e51f01888f5aa08e51fa47533000e4f33ca1
Nayan <chandrakarnayan96@gmail.com>
2024-07-31 18:42:28 +0530

    Update MiniNavbar.tsx

--- components/Header/mobile/MiniNavbar.tsx
+++ components/Header/mobile/MiniNavbar.tsx
@@ -1,56 +1,72 @@
 "use client"
-import { AiOutlineCloseCircle } from "react-icons/ai"
-import { navigation } from "@constants/navigation"
-import { FC, useCallback } from "react"
-import { useRouter } from "next-nprogress-bar"
-import Link from "next/link"
-import HoverButton from "@components/ui/button/HoverButton"
+import { FC } from "react"
+import { useRouter } from "next/navigation"
+
+import { navigation } from "@/constants/navigation"
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/ui/sheet"
+import useMenu from "@/hooks/useMenu"
+import { Button } from "@/components/ui/button/Button"
+import { Sparkles } from "lucide-react"
 
 
 interface MiniNavbarProps {
-    isOpen: boolean,
-    handleClick: () => void
+
 }
 
-const MiniNavbar: FC<MiniNavbarProps> = ({ isOpen, handleClick }) => {
+const MiniNavbar: FC<MiniNavbarProps> = ({ }) => {
     const router = useRouter()
-    const handleRoute = useCallback(
-        (href: string) => {
-            router.push(href)
-        },
-        [handleClick, router]
-    )
+    const mobileMenu = useMenu()
+
+    const handleRoute = (href: string) => {
+        mobileMenu?.onClose()
+        router.push(href)
+
+    }
+
+
 
     return (
-        <div
-            className={`
-              bg-black fixed translate xl:hidden inset-0 w-[18rem] h-[100vh] p-6 md:p-4 transition-all duration-900 ${isOpen ? "translate-x-0" : "-translate-x-96"} ${isOpen ? "opacity-100" : "opacity-20"}`}
+        <Sheet
+            open={mobileMenu?.isOpen}
+            onOpenChange={() => mobileMenu?.onClose()}
+
         >
-            <span
-                onClick={() => handleClick()}
-                className="cursor-pointer p-2">
-                <AiOutlineCloseCircle size={30} color="white" />
-            </span>
-            <div
-                className=" flex justify-start gap-3 flex-col h-full mt-4 w-full">
-                {navigation?.map((data) => {
-                    const { href, id, label, svg } = data
+            <SheetContent
+                side="left"
+                className="bg-black border-r border-zinc-800 w-full"
+            >
+
+          <SheetHeader className="text-white text-start">
+            <SheetTitle className="text-transparent bg-clip-text bg-gradient_blue" >Menu</SheetTitle>
+          </SheetHeader>
+
+
+            <div className="flex gap-2 justify-between flex-col h-full">
+            <div className="flex items-start gap-4 flex-col mt-6">
+            {navigation?.map((data) => {
+                    const { href, id, label , Icon } = data
                     return (
                         <span
                             key={id}
-                            className=" text-sm text-white bg-transparent transition-all duration-200 rounded-[10px] p-4 cursor-pointer border-[1.5px] border-gray-500 hover:border-white"
+                            className=" text-sm text-transparent bg-clip-text bg-gradient_green bg-transparent w-full transition-all duration-200 rounded-[4px] px-3 py-2 cursor-pointer border border-zinc-800 flex items-center font-semibold"
                             onClick={() => handleRoute(href)}
                         >
+                            <Icon className="size-5 mr-2 text-white" />
                             {label}
                         </span>
                     )
                 })}
-                <Link target='_blank' rel="noreferer" href="mailto:nayangit000@gmail.com" className="mt-6 text-center" >
-                    <HoverButton
-                        label="Hire Me" />
-                </Link>
-            </div >
-        </div >
+            </div>
+
+            <Button
+                className="w-full bg-gradient_blue_purple rounded-[4px] mb-5"
+            >
+            <Sparkles className="size-5 fill-white mr-2" />
+             Hire Me   
+            </Button>
+            </div>
+            </SheetContent>
+        </Sheet>
     )
 }
 export default MiniNavbar
